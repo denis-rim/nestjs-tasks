6a305f59cb4ec539d8b182f37615ce9e9ab490a4
Denis Mazurchuk <denis.aurorim@gmail.com>
2021-11-09 21:58:57 +0200

    Restricting Getting a Task By ID

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -18,8 +18,9 @@ export class TasksService {
     return this.tasksRepository.getTasks(filterDto, user);
   }
 
-  async getTaskById(id: string): Promise<Task> {
-    const found = await this.tasksRepository.findOne(id);
+  async getTaskById(id: string, user: User): Promise<Task> {
+    // const found = await this.tasksRepository.findOne(id);
+    const found = await this.tasksRepository.findOne({ where: { id, user } });
 
     if (!found) {
       throw new NotFoundException(`Task with ID "${id}" not found`);
@@ -39,12 +40,12 @@ export class TasksService {
     }
   }
 
-  async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
-    const task = await this.getTaskById(id);
-
-    task.status = status;
-    await this.tasksRepository.save(task);
-
-    return task;
-  }
+  // async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
+  //   const task = await this.getTaskById(id);
+  //
+  //   task.status = status;
+  //   await this.tasksRepository.save(task);
+  //
+  //   return task;
+  // }
 }
