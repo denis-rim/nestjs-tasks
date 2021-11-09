674aed03d6ad06673c343743e38b6af6ea4f6b39
Denis Mazurchuk <denis.aurorim@gmail.com>
2021-11-09 12:33:22 +0200

    Creating tasks repository

---
+++ src/tasks/tasks.repository.ts
@@ -0,0 +1,8 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { Task } from './task.entity';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatus } from './task.model';
+
+@EntityRepository(Task)
+export class TasksRepository extends Repository<Task> {}
